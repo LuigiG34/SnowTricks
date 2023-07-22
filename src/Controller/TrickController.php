ec2ff7bd5f60c996d8b643acaa91d8eca391f2b4
Luigi Gandemer <luigigandemer6@gmail.com>
2023-07-22 18:25:00 +0200

    add a comment

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -2,31 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\CommentType;
 use App\Repository\TrickRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TrickController extends AbstractController
 {
+    
     #[Route('/tricks/{slug}', name: 'app_trick')]
-    public function index($slug, TrickRepository $repository): Response
+    public function index($slug, TrickRepository $repository, Request $request, EntityManagerInterface $manager): Response
     {
         $unique = $repository->findOneBy(['slug' => $slug]);
+        $user = $this->getUser();
+
+        $form = $this->createForm(CommentType::class);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+
+            $comment = new Comment;
+            $comment->setContent($data->getContent());
+            $comment->setTrick($unique);
+            $comment->setCreatedAt(new \DateTimeImmutable('now'));
+            $comment->setUser($user);
+            $manager->persist($comment);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_trick', ['slug' => $unique->getSlug()]);
+        }
 
         return $this->render('trick/index.html.twig', [
+            'form' => $form->createView(),
             'trick' => $unique,
         ]);
     }
+
+
+
+
 
     #[Route('/get-more-tricks/{offset}/{limit}', name: 'get_more_tricks', methods: ['GET'])]
     public function getMoreTricks(TrickRepository $repository, $offset, $limit): JsonResponse
     {
         $all = $repository->findAll();
-
-
 
         $tricksData = [];
         foreach ($all as $trick) {
