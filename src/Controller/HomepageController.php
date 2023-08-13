6b95395dae1c3ab4d8257e8ce10857661de5991f
Luigi Gandemer <luigigandemer6@gmail.com>
2023-08-13 15:59:28 +0200

    Pagination

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -12,10 +12,84 @@ class HomepageController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function index(TrickRepository $repository): Response
     {
-        $tricks = $repository->findBy([],['createdAt' => 'ASC'], 9);
+        $paginator = $repository->getTrickPaginator(0);
 
         return $this->render('homepage/index.html.twig', [
-            'tricks' => $tricks,
+            'tricks' => $paginator,
         ]);
     }
+
+
+    #[Route('/get-more-tricks/{offset}', name: 'get_more_tricks', methods: ['GET'])]
+    public function getMoreTricks(TrickRepository $repository, $offset)
+    {
+        $paginator = $repository->getTrickPaginator($offset);
+        $html="";
+
+        foreach ($paginator as $trick) {
+            $firstImage = null;
+            $images = $trick->getImages();
+            if (!empty($images)) {
+                $firstImage = $images[0]->getPath();
+            } else {
+                $firstImage = "/assets/img/default.jpg";
+            }
+            
+            $html .= "
+            <div class='col-s-12 col-m-6 col-lg-4 trick'>
+                <div class='card bg-light shadow m-5'>
+                    <img src='".$firstImage."' class='card-img-top img-trick' alt='...'>
+                    <div class='card-body'>
+                        <h5 class='card-title'>
+                            <div class='d-flex justify-content-between align-items-center'>
+                                <a class='btn btn-primary' href='/tricks/".$trick->getSlug()."'>
+                                    ".$trick->getName()."</a>";
+            
+            if($this->getUser()) {
+                $html .= "<div>
+                <a class='btn btn-light' href='{{ path('/tricks/edit/".$trick->getSlug()."'>
+                    <i class='bi bi-pencil-fill'></i>
+                </a>
+                <button type='button' class='btn btn-light' data-bs-toggle='modal' data-bs-target='#exampleModal".$trick->getId()."'>
+                    <i class='bi bi-trash-fill'></i>
+                </button>
+                </div>
+                <div class='modal fade' id='exampleModal".$trick->getId()."' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                    <div class='modal-dialog'>
+                        <div class='modal-content'>
+                            <div class='modal-header'>
+                                <h5 class='modal-title' id='exampleModalLabel'>Delete '".$trick->getName()."'
+                                </h5>
+                                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                            </div>
+                            <div class='modal-body'>
+                                Are you sure you want to delete '".$trick->getName()."' ?
+                            </div>
+                            <div class='modal-footer'>
+                                <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Cancel</button>
+                                <form method='post' action='/tricks/delete/".$trick->getSlug()."'>
+                                    <input type='hidden' name='_method' value='DELETE'>
+                                    <input type='hidden' name='_token' value='{{ csrf_token('delete' ~ ".$trick->getSlug().") }}'>
+                                    <button class='btn btn-danger'>Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>";
+            }
+
+            $html .= "
+                            </div>
+                        </h5>
+                    </div>
+                </div>
+            </div>";                            
+        }
+
+        return new Response($html);
+    }
 }
+
+
+
+
--- src/Repository/TrickRepository.php
+++ src/Repository/TrickRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Trick;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 
 /**
  * @extends ServiceEntityRepository<Trick>
@@ -16,9 +17,23 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class TrickRepository extends ServiceEntityRepository
 {
+    public const PAGINATOR_PER_PAGE = 9;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Trick::class);
+    }
+
+    public function getTrickPaginator(int $offset): Paginator
+    {
+        $query = $this->createQueryBuilder('t')
+            ->orderBy('t.createdAt', 'DESC')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
+            ->setFirstResult($offset)
+            ->getQuery()
+        ;
+
+        return new Paginator($query);
     }
 
     public function save(Trick $entity, bool $flush = false): void
