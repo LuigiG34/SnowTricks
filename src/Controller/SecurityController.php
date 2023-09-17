12036b95d67749eb6a24a8b7521bdabe6d29d013
Luigi Gandemer <luigigandemer6@gmail.com>
2023-09-17 18:41:55 +0200

    Fixed bug priority of routes

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login', methods: ['POST'])]
+    #[Route(path: '/login', name: 'app_login', methods: ['GET','POST'], priority: 10)]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
